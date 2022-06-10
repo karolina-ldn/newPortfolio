98926f4a9aada68cad48d3156660c57d84cb69c7
karolina-ldn <dampers00_acetic@icloud.com>
2022-06-10 13:19:03 +0100

    js

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const sections = document.querySelectorAll(".section");
+const secBtns = document.querySelectorAll(".controls");
+const secBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function pageTransition(){
+    // button click active class
+    for (let i = 0; i < secBtn.length; i++){
+        secBtn[i].addEventListener("click", function() {
+            let currentBtn = document.querySelectorAll(".active-btn");
+            currentBtn[0].className = currentBtn[0].className.replace("active-btn","");
+            this.className += " active-btn";
+        })
+    }
+}
+
+pageTransition();
+
